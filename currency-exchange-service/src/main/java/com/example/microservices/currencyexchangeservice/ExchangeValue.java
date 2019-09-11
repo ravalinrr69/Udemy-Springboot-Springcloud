01434196efcec59b702b391c2c89de02751276d7
rnagabandi <rnagabandi@USA-NIS-1899.local>
2019-09-10 17:31:13 -0700

    Adds microservice which implements currency exchange

---
+++ currency-exchange-service/src/main/java/com/example/microservices/currencyexchangeservice/ExchangeValue.java
@@ -0,0 +1,62 @@
+package com.example.microservices.currencyexchangeservice;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class ExchangeValue {
+
+	@Id
+	private Long id;
+	@Column(name="currency_from")
+	private String from;
+	@Column(name="currency_to")
+	private String to;
+	private BigDecimal conversionMultiple;
+	private int port;
+	
+	public ExchangeValue() {
+		super();
+	}
+	public ExchangeValue(Long id, String from, String to, BigDecimal conversionMultiple) {
+		super();
+		this.from = from;
+		this.to = to;
+		this.id = id;
+		this.conversionMultiple = conversionMultiple;
+	}
+	public String getFrom() {
+		return from;
+	}
+	public void setFrom(String from) {
+		this.from = from;
+	}
+	public String getTo() {
+		return to;
+	}
+	public void setTo(String to) {
+		this.to = to;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public BigDecimal getConversionMuliple() {
+		return conversionMultiple;
+	}
+	public void setConversionMuliple(BigDecimal conversionMultiple) {
+		this.conversionMultiple = conversionMultiple;
+	}
+	public int getPort() {
+		return port;
+	}
+	public void setPort(int port) {
+		this.port = port;
+	}
+	
+}
---
+++ currency-exchange-service/src/main/resources/data.sql
@@ -0,0 +1,2 @@
+insert into exchange_value(id, currency_from, currency_to, conversion_multiple, port) values (1001,'USD','INR',65,0);
+insert into exchange_value(id, currency_from, currency_to, conversion_multiple, port) values (1002,'AUS','INR',45,0);
